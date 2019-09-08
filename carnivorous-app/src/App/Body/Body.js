d430c9da3898c4d7c829f346ce815611539e5841
Inh3ritance <herreralaces@gmail.com>
2019-09-08 02:20:18 -0700

    Structured Site Design
    Created nav class to fix with webscrolling, designed left nav, and content space

--- carnivorous-app/src/App/Body/Body.js
+++ carnivorous-app/src/App/Body/Body.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import './Body.scss';
 
+import venus from './Images/Test.jpg'
+
 function Body() {
   return (
     <div className="Body">
-		<div className="Carousel-Container">
+		<div className="Carousel-Container parrallax">
+			<img src= {venus} />
 		</div>
 		<div className="Container">
 			<div className= "Left-Nav">
-				<h1>Link</h1>
-				<h1>Link</h1>
-				<h1>Link</h1>
-				<h1>Link</h1>
+				<h1>Home</h1>
+				<h1>Colas & Hardy</h1>
+				<h1>Tropical</h1>
+				<h1>Byblis</h1>
+				<h1>Nepenthes</h1>
 			</div>
 			<div className = "Content">
 				<h1>Right</h1>
--- carnivorous-app/src/App/Nav/Nav.js
+++ carnivorous-app/src/App/Nav/Nav.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import './Nav.scss';
 
-function Nav() {
-  return (
-    <div className="Nav">
-			<h1 id = "Company_Name">LA CARNIVORES</h1>
-			<div id = "Right_Buttons">
-				
-				<h2>Search</h2>
-				<h2>shopping cart image</h2>
-			</div>
-    </div>
-  );
+class Nav extends React.Component {
+  constructor(props) {
+        super(props);
+
+        this.state = {};
+
+        this.handleScroll = this.handleScroll.bind(this);
+  }
+
+    handleScroll() {
+        this.setState({scroll: window.scrollY});
+  }
+  
+  componentDidMount() {
+        const move = document.querySelector('nav');
+        this.setState({top: move.offsetTop, height: move.offsetHeight});
+        window.addEventListener('scroll', this.handleScroll);
+  }
+  
+  componentDidUpdate() {
+        this.state.scroll > this.state.top ? 
+            document.body.style.paddingTop = `${this.state.height}px` :
+            document.body.style.paddingTop = 0;
+  }
+    
+  render() {
+    return (
+    <nav className={this.state.scroll > this.state.top ? "fixed-nav" : ""}>
+		<h1 id = "Company_Name">LA CARNIVORES</h1>
+		<div id = "Right_Buttons">
+		
+			<h2>Search</h2>
+			<h2>shopping cart image</h2>
+		</div>
+	</nav>
+    );
+  }
+
 }
 
 export default Nav;
