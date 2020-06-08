b21f83b58eda97eb74b791632a1de85b5101bd6c
Kenny Chhoeun <kchhoeun20703@gmail.com>
2020-06-08 14:25:25 -0700

    BurgerMenu, reinstall bootsrap, uninstall mysql

--- carnivorous-app/src/App/Nav/Nav.js
+++ carnivorous-app/src/App/Nav/Nav.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import { pushRotate as Menu } from 'react-burger-menu';
 import { BrowserView, MobileView } from 'react-device-detect';
 import './Nav.scss';
-import {Elements, StripeProvider} from 'react-stripe-elements';
+import { Elements, StripeProvider } from 'react-stripe-elements';
 import CheckoutForm from '../Body/Checkout/CheckoutForm.js';
 import {
   BrowserRouter as Router,
@@ -13,66 +14,73 @@ import {
 import Body from '../Body/Body.js';
 import Footer from '../Footer/Footer.js';
 
-
 class Nav extends React.Component {
 
   /* Constructor */
   constructor(props) {
-        super(props);
-        this.state = { };
-        this.handleScroll = this.handleScroll.bind(this);
+    super(props);
+    this.state = {};
+    this.handleScroll = this.handleScroll.bind(this);
+  }
+
+  showSettings (event) {
+    event.preventDefault();
   }
 
   /* Function handling/setting scroll state*/
   handleScroll() {
-        this.setState({scroll: window.scrollY});
+    this.setState({ scroll: window.scrollY });
   }
 
   /* Immediatly set the state for scroll */
   componentDidMount() {
-        const move = document.querySelector('nav');
-        this.setState({top: move.offsetTop, height: move.offsetHeight});
-        window.addEventListener('scroll', this.handleScroll);
+    const move = document.querySelector('nav');
+    this.setState({ top: move.offsetTop, height: move.offsetHeight });
+    window.addEventListener('scroll', this.handleScroll);
   }
 
   /* Update scroll position after mounting */
   componentDidUpdate() {
-        this.state.scroll > this.state.top ? 
-            document.body.style.paddingTop = `${this.state.height}px` :
-            document.body.style.paddingTop = 0;
+    this.state.scroll > this.state.top ?
+      document.body.style.paddingTop = `${this.state.height}px` :
+      document.body.style.paddingTop = 0;
   }
 
   render() {
     return (
-        <div>
-          <Router>
-            <nav className={this.state.scroll > this.state.top ? "fixed-nav" : ""}>
-              <Link to = '/'><h1 id = "Company_Name">LA Carnivores</h1></Link>
-              <BrowserView viewClassName = "Right_Buttons">
-                  <NavLink to = '/Checkout' activeClassName="checkout_render">
-                    <button className="btn btn-info btn-lg" id="cart-overlay">
-                      <h2 id = "cart"><span className="glyphicon glyphicon-shopping-cart"> Cart 0</span></h2>
-                    </button>
-                  </NavLink>
-              </BrowserView>
-              <MobileView viewClassName = "Right_Buttons">
-              </MobileView>
-            </nav>
-            <Switch>
-                <Route exact path='/'><div><Body/><Footer/></div></Route>
-                <Route path='/Checkout'>
-                  <div>
-                  <StripeProvider apiKey="pk_test_Mg00XTISPu5dW10aHJI9IfVq00pOUm5l4g">
-                    <Elements>
-                      <CheckoutForm />
-                    </Elements>
-                  </StripeProvider>
-                  <Footer/>
-                  </div>
-                </Route>
-            </Switch>
-          </Router>
-        </div>
+      <div>
+        <Router>
+          <nav className={this.state.scroll > this.state.top ? "fixed-nav" : ""}>
+            <Link to='/'><h1 id="Company_Name">LA Carnivores</h1></Link>
+            <BrowserView viewClassName="Right_Buttons">
+              <NavLink to='/Checkout' activeClassName="checkout_render">
+                <button className="btn btn-info btn-lg" id="cart-overlay">
+                  <h2 id="cart"><span className="glyphicon glyphicon-shopping-cart"> Cart 0</span></h2>
+                </button>
+              </NavLink>
+            </BrowserView>
+            <MobileView viewClassName="Right_Buttons">
+            <Menu right>
+                <a id="home" className="menu-item" href="/">Home</a>
+                <a id="checkout" className="menu-item" href="/Checkout">Checkout</a>
+              </Menu>
+            </MobileView>
+          </nav>
+          <Switch>
+            <Route exact path='/'><div><Body /><Footer /></div></Route>
+            <Route path='/Checkout'>
+              <div>
+                <StripeProvider apiKey="pk_test_Mg00XTISPu5dW10aHJI9IfVq00pOUm5l4g">
+                  <Elements>
+                    <CheckoutForm />
+                  </Elements>
+                </StripeProvider>
+                <Footer />
+              </div>
+            </Route>
+          </Switch>
+        </Router>
+      </div>
     );
   }
 
