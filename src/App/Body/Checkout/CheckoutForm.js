999707d0e8c1fda02e9a88ba122be662c7416e8d
Inh3ritance <herreralaces@gmail.com>
2021-04-26 18:10:34 -0700

    Fix checkout bug

--- src/App/Body/Checkout/CheckoutForm.js
+++ src/App/Body/Checkout/CheckoutForm.js
@@ -100,7 +100,7 @@ class CheckoutForm extends Component {
         /* Temporary solution */
         const card = document.getElementsByTagName(CardElement);
         const result = await this.props.stripe.createToken(card);
-        if(result.error.message) {
+        if(result.error.message !== undefined) {
             toast(result.error.message, { type: 'error' });
             this.setState({ disable: false });
             return;
