1a996f7601bc23743fa63cb1495611b27439cd32
Inh3ritance <herreralaces@gmail.com>
2021-05-21 11:17:46 -0700

    Rename sign up/sign in

--- src/App/Nav/Nav.js
+++ src/App/Nav/Nav.js
@@ -147,7 +147,7 @@ class Nav extends React.Component {
             <div className="Right_Buttons_mobile notMobile" activeclassname="checkout_render">
               <Menu right isOpen={this.state.menuOpen} onStateChange={(state) => this.handleStateChange(state)} className='Menu'>
                 <Link id="home" className="menu-item" to="/"><ToggleSwitch Selector={this.onChangeSelector.bind(this)} /></Link>
-                <button onClick={()=>this.signIn()}><h2 id="user_mobile">Users</h2></button>
+                <button onClick={()=>this.signIn()}><h2 id="user_mobile">Sign-in</h2></button>
                 <NavLink id="cart" className="menu-item" activeClassName="checkout_render" to="/Checkout">
                 <button className="btn btn-info btn-lg" id="cart-overlay" onClick={this.closeMenu}>
                   <h2 id="cart"><span className="glyphicon glyphicon-shopping-cart"> Cart {this.state.total}</span></h2>
