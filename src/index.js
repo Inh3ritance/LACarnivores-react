06f22fd9d379f707d2d1377829c94f853666d85d
Inh3ritance <herreralaces@gmail.com>
2021-07-03 14:19:22 -0700

    Add google analytics
    Also update packages for vulnerabilities

--- src/index.js
+++ src/index.js
@@ -4,8 +4,15 @@ import './index.css';
 import App from './App/App';
 import * as serviceWorker from './serviceWorker';
 import netlifyIdentity from 'netlify-identity-widget';
+import ReactGA from 'react-ga';
 
+// Utilize google analytics
+ReactGA.initialize('UA-201254265-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
+
+// Utilize netlify identity
 netlifyIdentity.init();
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
